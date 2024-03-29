fdff24722bb8b8753d42add25b0963cfb103ae61
Yurii Zhuk <iamyuriizr@gmail.com>
2024-03-29 16:57:45 +0200

    Initial Commit

---
+++ demo-app-ios/demo_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_app_iosApp.swift
+//  demo-app-ios
+//
+//  Created by Yurii Zhuk on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct demo_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
